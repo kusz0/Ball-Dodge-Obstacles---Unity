88bdee02eba335f8d0d9279566d92181c98fcc36
Kacper Kuszek <kuszekkacper@gmail.com>
2026-03-18 13:07:10 +0100

    fly ball at player

--- Assets/Scripts/Spinner.cs
+++ Assets/Scripts/Spinner.cs
@@ -5,12 +5,15 @@ using UnityEngine.UIElements;
 public class Spinner : MonoBehaviour
 {
 
+    [SerializeField] float xAngle = 0;
+    [SerializeField] float yAngle = 0;
+    [SerializeField] float zAngle = 0;
     private void Start()
     {
         
     }
     void Update()
     {
-        transform.Rotate(0, 0, 1);
+        transform.Rotate(xAngle, yAngle, zAngle);
     }
 }
