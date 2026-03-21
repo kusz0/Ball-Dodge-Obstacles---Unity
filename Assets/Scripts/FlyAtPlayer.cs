1199c40c2bd65766dc92b6a0a868a01371636b10
Kacper Kuszek <kuszekkacper@gmail.com>
2026-03-21 17:49:34 +0100

    destroy object

---
+++ Assets/Scripts/FlyAtPlayer.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class FlyAtPlayer : MonoBehaviour
+{
+    [SerializeField] float speed = 1f;
+    [SerializeField] Transform player;
+    Vector3 playerPosition;
+
+    void Start()
+    {
+        playerPosition = player.transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, playerPosition, speed * Time.deltaTime);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
